6531fe68551c26ea29704fe7cbf4c7928d161c21
Vamsi <vamsir@gmail.com>
2020-05-28 00:06:58 -0400

    format column names

--- oi.py
+++ oi.py
@@ -129,7 +129,8 @@ with open(fname+'-summary.csv', "rb") as f:
     s3.upload_fileobj(f, "tsla-oi",'%s/%s' % ('summary',datestr+'-summary.csv'))
     
 pivtable_expiry = pd.pivot_table(conSum,values=['netHedge'],index=['Expiry'], columns=['Price'], aggfunc=np.sum)
-
+#round the column names to be more legible
+pivtable_expiry.columns = list(np.around(np.array(pivtable_expiry.columns),0))
 #sumarize for consumption
 pivtable = pd.pivot_table(conSum,values=['netHedge'], columns=['Price'], aggfunc=np.sum)
 #calculate change in hege need
